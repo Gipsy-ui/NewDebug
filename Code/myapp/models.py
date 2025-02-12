7d47ea0fe54f07c73d341f32d00a0c6ce1abbd0d
Gipsy <187034530+Gipsy-ui@users.noreply.github.com>
2025-02-12 08:30:26 +0800

    Added Connections to Db

--- Code/myapp/models.py
+++ Code/myapp/models.py
@@ -1,20 +1,18 @@
 from django.db import models
 
 class Animals(models.Model):
-    name = models,models.CharField(("Max_length = 100"), max_length=100)
-    Species = models.CharField(("Max_length = 100"), max_length=100)
+    name = models.CharField(max_length=100, default="Unknown")  
+    species = models.CharField(max_length=100, default="Unknown")  
     age = models.IntegerField()
-    discription = models.TextField(blank = True, null=True)
+    description = models.TextField(null=True, blank=True)  
 
     def __str__(self):
         return self.name
-    
+
 class Place(models.Model):
-    name = models.CharField(("max_length = 100"), max_length=100)
-    location = models.CharField(max_length = 100)
-    description = models.TextField(blank = True, null=True)
+    name = models.CharField(max_length=100, default="Unknown")  
+    location = models.CharField(max_length=100, default="Unknown")  
+    description = models.TextField(null=True, blank=True)  
 
     def __str__(self):
         return self.name
-    
-# Create your models here.
--- Code/myapp/views.py
+++ Code/myapp/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render, redirect
 from .models import Animals, Place
 
 def index(request):
+    if request.method == "POST":
+        form_type = request.POST.get("form_type")
+
+        if form_type == "animal":
+            animal_name = request.POST.get("animal_name")
+            species = request.POST.get("species")
+            age = request.POST.get("age")
+            description = request.POST.get("description")
+
+            if animal_name and species and age:
+                Animals.objects.create(
+                    name=animal_name, species=species, age=age, description=description
+                )
+
+        elif form_type == "place":
+            place_name = request.POST.get("place_name")
+            location = request.POST.get("location")
+            place_description = request.POST.get("place_description")
+
+            if place_name and location:
+                Place.objects.create(
+                    name=place_name, location=location, description=place_description
+                )
+
+        return redirect("index")
+
     animals = Animals.objects.all()
     places = Place.objects.all()
-
-    return render(request, 'create.html', {"animals": animals, "places": places}) # Don't Include myapp/ in the path
+    return render(request, "create.html", {"animals": animals, "places": places})
