6af9db84b4018f7609cf37c750f956d1d07aa767
Gipsy <187034530+Gipsy-ui@users.noreply.github.com>
2025-02-16 23:40:55 +0800

    Error in save & delete

--- Code/myapp/urls.py
+++ Code/myapp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('save/', views.save, name='save'),
+    path('edit/<str:model_type>/<int:id>/', views.edit, name='edit'),
+    path('update/<str:model_type>/<int:id>/', views.update, name='update'),  # Updated for both models
+    path('delete/<str:model_type>/<int:id>/', views.delete, name='delete'),
 ]
--- Code/myapp/views.py
+++ Code/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Animals, Place
 
 def index(request):
@@ -31,3 +31,24 @@ def index(request):
     animals = Animals.objects.all()
     places = Place.objects.all()
     return render(request, "create.html", {"animals": animals, "places": places})
+
+def update(request, model_type, id):
+    if request.method == "POST":
+        if model_type == "animal":
+            item = get_object_or_404(Animals, id=id)
+            item.name = request.POST.get('name')
+            item.species = request.POST.get('species')
+            item.age = request.POST.get('age')
+            item.description = request.POST.get('description')
+        elif model_type == "place":
+            item = get_object_or_404(Place, id=id)
+            item.name = request.POST.get('name')
+            item.location = request.POST.get('location')
+            item.description = request.POST.get('description')
+        else:
+            return render(request, 'error.html', {'message': 'Invalid model type'})
+
+        item.save()
+        return redirect('index')
+
+    return redirect('index')
